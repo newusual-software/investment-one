bd635c10791c5b1dee44f46f3a23b6fa224fc112
dev-phantom <godsfavour1975@gmail.com>
2023-12-26 14:46:27 -0800

    feat: added analyst view

--- src/components/landingSection/macroEconomics.jsx
+++ src/components/landingSection/macroEconomics.jsx
@@ -1,3 +1,4 @@
+import AnalystView from "../molecule/macroEconomics/analystView";
 import { TabsWithIcon } from "../molecule/tab/tab";
 
 export default function MacroEconomics() {
@@ -5,29 +6,33 @@ export default function MacroEconomics() {
     <div className="flex my-10 gap-4 flex-row w-full">
       <div className="w-[60%] flex flex-col gap-10 items-start">
         <div className="text-center text-neutral-800 text-[32px] font-medium font-workSans">
-          Macro-Economics 
+          Macro-Economics
         </div>
-        <div >
+        <div>
           <TabsWithIcon />
         </div>
       </div>
-      <div className=" w-[40%] justify-between gap-8 items-start flex">
-        <div className="grow shrink basis-0 justify-start items-center gap-1 flex">
-          <div>
-            <img
-              src="https://res.cloudinary.com/phantom1245/image/upload/v1703202730/investment-one/openmoji_wifi_ixewgq.png"
-              alt="wifi"
-            />
+      <div className=" w-[40%] ">
+        <div className=" justify-between gap-8 my-2 items-start flex">
+          <div className="grow shrink basis-0 justify-start items-center gap-1 flex">
+            <div>
+              <img
+                src="https://res.cloudinary.com/phantom1245/image/upload/v1703202730/investment-one/openmoji_wifi_ixewgq.png"
+                alt="wifi"
+              />
+            </div>
+            <div className="text-center text-neutral-800 text-[22px] font-medium font-workSans">
+              Analyst View
+            </div>
           </div>
-          <div className="text-center text-neutral-800 text-[22px] font-medium font-workSans">
-            Analyst View
-          </div>
+          <button className="px-5 border-none outline-non py-2.5 bg-orange rounded-[10px] justify-center items-center gap-2 flex">
+            <div className="text-center text-white text-base font-normal font-workSans leading-snug tracking-wide">
+              View All
+            </div>
+          </button>
+          <br />
         </div>
-        <button className="px-5 border-none outline-non py-2.5 bg-orange rounded-[10px] justify-center items-center gap-2 flex">
-          <div className="text-center text-white text-base font-normal font-workSans leading-snug tracking-wide">
-            View All
-          </div>
-        </button>
+        <AnalystView />
       </div>
     </div>
   );
