0185e8852bffdf9e2b32e0b90fe4256a0b8e9a5c
dev-phantom <godsfavour1975@gmail.com>
2023-12-27 16:13:01 +0100

    feat: added banking to equities

--- src/components/charts/nse-asi.jsx
+++ src/components/charts/nse-asi.jsx
@@ -1,8 +1,7 @@
 import { Line } from 'react-chartjs-2';
-import 'chartjs-adapter-date-fns';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Filler, Legend } from 'chart.js/auto';
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, 'chartjs-adapter-date-fns', Title, Tooltip, Filler, Legend);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement,  Title, Tooltip, Filler, Legend, );
 
 export default function NSEASIChart() {
   const smooth = true; // Set to true for smooth tension, false for no tension
@@ -17,23 +16,7 @@ export default function NSEASIChart() {
         text: 'Chart.js Line Chart',
       },
     },
-    scales: {
-        x: {
-            type: 'time',
-            time: {
-              parser: 'dd/MM/yyyy',
-              unit: 'day',
-              unitStepSize: 1,
-              displayFormats: {
-                day: 'dd/MM/yyyy',
-              },
-            },
-            title: {
-              display: true,
-              text: 'Date',
-            },
-          },
-    },
+    
     elements: {
       line: {
         tension: smooth ? 0.5 : 0,
@@ -56,6 +39,7 @@ export default function NSEASIChart() {
     "23809.31", "23809.31", "23089.86", "23089.86", "23089.86"
   ];
 
+  // Create a map to store unique x-axis labels and corresponding y-axis values
 
   // Convert the map into an array for Chart.js data format
   const data = {
@@ -73,6 +57,6 @@ export default function NSEASIChart() {
       },
     ],
   };
-
+  
   return <Line options={options} data={data} />;
 }
